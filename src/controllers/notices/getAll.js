f5015e7de40997ef78d30528159009938c30be14
NataliiaKolomiichenko <gella1996nr@gmail.com>
2023-02-03 22:53:50 +0200

    fix swagger and added getAll

--- src/controllers/notices/getAll.js
+++ src/controllers/notices/getAll.js
@@ -1,9 +1,10 @@
 const services = require('../../services/notices');
 
 const getAll = async (req, res) => {
+    const {page = 1, limit = 8} = req.query;
     const {category} = req.params;
 
-    const notices = await services.getAll(category);
+    const notices = await services.getAll(category, page, limit);
 
     res.status(200).json(notices);
 };
