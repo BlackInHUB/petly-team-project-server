6b056a027a517fd4881f184d3e65c6d71ab82cd9
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-03 20:24:15 +0200

    Add pagination to getAll notices

--- src/services/notices/getAll.js
+++ src/services/notices/getAll.js
@@ -1,7 +1,11 @@
 const {Notice} = require('../../models');
 
-const getAll = async (category) => {
-    return await Notice.find({category});
+const getAll = async (category, page, limit) => {
+    const skip = (page - 1) * limit;
+    
+    return await Notice.find({category})
+        .skip(skip)
+        .limit(limit);
 };
 
 module.exports = getAll;
