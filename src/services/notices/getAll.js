ed71f633e33597cccd0b68252fc23ef96af00c46
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-06 15:45:42 +0200

    Update getAll.js
    add sort by created date

--- src/services/notices/getAll.js
+++ src/services/notices/getAll.js
@@ -4,12 +4,14 @@ const getAll = async (category, page, limit, filter) => {
     const skip = (page - 1) * limit;
 
     if (filter !== '') {
-        return await Notice.find({category,  title: {$regex: filter, "$options": 'i'}});
+        return await Notice.find({category,  title: {$regex: filter, "$options": 'i'}})
+        .sort({createdAt: -1});
     };
     
     return await Notice.find({category})
         .skip(skip)
-        .limit(limit);
+        .limit(limit)
+        .sort({createdAt: -1});
 };
 
 module.exports = getAll;
