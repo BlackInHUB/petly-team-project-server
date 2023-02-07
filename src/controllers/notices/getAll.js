9a9825963b7c23dfcc063a01aa799b9b89cddf73
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-07 18:10:46 +0200

    fix getAll

--- src/controllers/notices/getAll.js
+++ src/controllers/notices/getAll.js
@@ -1,11 +1,9 @@
 const services = require('../../services/notices');
 
 const getAll = async (req, res) => {
-    const {page = 1, limit = 8} = req.query;
     const {category} = req.params;
-    const {filter = ''} = req.query;
 
-    const notices = await services.getAll(category, page, limit, filter);
+    const notices = await services.getAll(category, req.query);
 
     res.status(200).json(notices);
 };
--- src/services/notices/getAll.js
+++ src/services/notices/getAll.js
@@ -1,16 +1,12 @@
 const {Notice} = require('../../models');
 
-const getAll = async (category, page, limit, filter) => {
-    const skip = (page - 1) * limit;
-
-    if (filter !== '') {
-        return await Notice.find({category,  title: {$regex: filter, "$options": 'i'}})
+const getAll = async (category, {filter}) => {
+    if (filter) {
+        return await Notice.find({category, title: {$regex: filter, "$options": 'i'}})
         .sort({createdAt: -1});
     };
     
     return await Notice.find({category})
-        .skip(skip)
-        .limit(limit)
         .sort({createdAt: -1});
 };
 
