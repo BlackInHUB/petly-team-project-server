bbc47e1372898fe458c5e9a0354842247db871ed
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-02-13 00:41:27 +0200

    Update get.js

--- src/services/messages/get.js
+++ src/services/messages/get.js
@@ -4,6 +4,7 @@ const get = async (userId) => {
     const messages = await User.findById(userId, {messages: 1, _id: 0}).populate({
         path: 'messages',
         model: 'Message',
+        options: {sort: {createdAt: -1}}
     })
 
     return messages;
